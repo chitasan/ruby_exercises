8c9b7bc384a50bb149d3b9729affdfc645564af1
Chi Tran <chidtran@gmail.com>
2019-01-28 08:50:49 -0700

    add Data-Types Hashes, and Objects and Methods Ex 1

---
+++ objects-and-methods/exercise-1/lib/bag.rb
@@ -0,0 +1,25 @@
+class Bag
+  attr_reader :count, :candies, :type
+
+  def initialize
+    @candies = []
+  end
+
+  def empty?
+    candies.empty?
+  end
+
+  def count
+    candies.count
+  end
+
+  def << candy
+    candies << candy
+  end
+
+  def contains?(type)
+    candies.any? do |candy|
+      candy.type == type
+    end
+  end
+end
